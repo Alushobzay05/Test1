6c50ab94435d0be0c20fbc87e2c9cdcc0abe2efc
Alushobzay05 <02220235.cst@rub.bt>
2024-02-15 16:15:50 +0600

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+print("2024 CSF Grade")
+
+grade = 85
+if grade >= 90:
+     print("Grade:A")
+elif grade >= 80:
+    print("Grade:B")
+elif grade >= 70:
+    print("Grade:C")
+elif grade >= 60:
+    print("Grade:D")             
+else:
+    print("Grade:F")    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+
+
+print("2024 CSF Grade")
+name = input ("What is your name")
+num = input("enter your grade")
+print("name")
+print("num")
+if grade >= 90:
+     print("Grade:A")
+elif grade >= 80:
+    print("Grade:B")
+elif grade >= 70:
+    print("Grade:C")
+elif grade >= 60:
+    print("Grade:D")             
+else:
+    print("Grade:F")    
