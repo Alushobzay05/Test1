c490ceea6cc3dae7550d096e62fc97e1a4c1aac2
Alushobzay05 <02220235.cst@rub.bt>
2024-02-29 11:32:23 +0600

    class work

---
+++ class_activity.py
@@ -0,0 +1,19 @@
+name = input("Enter your name:")
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+print("hello", name, "!")
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplicaton_result = num1 * num2
+division_result = num1 / num2
+
+print("the sum of ", num1, "and", num2, "is", addition_result)
+print("the difference of", num1, "and", num2, "is", subtraction_result)
+print("the product of", num1, "and", num2, "is", multiplicaton_result)
+print("the quotient", num1, "and", num2, "is", division_result)
+
+
+
+
+
