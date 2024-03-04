57048bd69e73911cd936d96de2c7c639e1efb65b
Alushobzay05 <02220235.cst@rub.bt>
2024-03-04 14:04:25 +0600

    lab work

--- exercise.py
+++ exercise.py
@@ -1,1 +1,13 @@
-print(input("Enter your age:"))
+age = int(input("Enter your age: "))
+
+is_student = input("Are you a student? (Yes/No): ")
+
+if age <= 12 and is_student == "yes":
+    print("You are eligible for a discount on ticket.")
+elif age < 13 or age <= 18:
+    print("You are eligible for a discount on ticket.")
+
+elif age > 12 or (is_student == "no"):
+    print("Sorry, you are not eligible for a discount on ticket.")
+else :
+    print("Entered input is invalid")
